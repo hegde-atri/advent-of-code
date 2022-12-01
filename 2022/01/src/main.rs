ef683f88a9b6b64f69910c8b4e348f1d8705b592
hegde-atri <iamatrihegde@outlook.com>
2022-12-01 11:28:21 +0000

    solved day 1

--- 2022/01/src/main.rs
+++ 2022/01/src/main.rs
@@ -1,3 +1,67 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    part1();
+    part2();
+}
+
+fn part1() {
+    let mut highest_calories = 0;
+    let mut sum: u32 = 0;
+    if let Ok(lines) = read_lines("./input.txt") {
+        for line in lines {
+            if let Ok(ip) = line {
+                if ip == "" {
+                    // If new line, check if Highest and reset sum
+                    if sum > highest_calories {
+                        highest_calories = sum
+                    }
+                    sum = 0;
+                } else {
+                    sum += ip.parse::<u32>().unwrap();
+                }
+            }
+        }
+    }
+    println!("Highest calories: {}", highest_calories);
+}
+
+fn part2() {
+    let mut top1: u32 = 0;
+    let mut top2: u32 = 0;
+    let mut top3: u32 = 0;
+    let mut sum: u32 = 0;
+    if let Ok(lines) = read_lines("./input.txt") {
+        for line in lines {
+            if let Ok(ip) = line {
+                if ip == "" {
+                    // If new line, check if Highest and reset sum
+                    if sum > top1 {
+                        top3 = top2;
+                        top2 = top1;
+                        top1 = sum;
+                    } else if sum > top2 {
+                        top3 = top2;
+                        top2 = sum;
+                    } else if sum > top3 {
+                        top3 = sum;
+                    }
+                    sum = 0;
+                } else {
+                    sum += ip.parse::<u32>().unwrap();
+                }
+            }
+        }
+    }
+    println!("Sum of Top 3 calories: {}", top1 + top2 + top3);
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
